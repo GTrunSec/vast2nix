62babf9c6981b6810d432c0f2e5d84219a1dfbc9
GTrunSec <gtrunsec@hardenedlinux.org>
2022-07-10 22:56:43 -0700

    Update

--- cells/user/packages/default.nix
+++ cells/user/packages/default.nix
@@ -9,8 +9,7 @@
 in {
   env = cells-lab._builder.library.mkPaths {
     name = "env";
-    paths =
-      [
+    paths = lib.optionals lock.deploy.config.vast.enable [
         # release latest bin
         inputs.cells.vast.packages.${lock.deploy.config.vast.version}
         # systemd.service
@@ -27,6 +26,7 @@ in {
       ++ lib.optionals lock.deploy.config.zeek.enable [
         __inputs__.hunting-cells.zeek.packages.${lock.deploy.config.zeek.version}
         __inputs__.hunting-cells.zeek.configFiles.pre-zeekctl
+        __inputs__.nixpkgs-hardenedlinux.packages.watchexec-simple
       ]
       ++ lib.optionals lock.deploy.config.threatbus.enable [
         __inputs__.hunting-cells.tenzir.packages.${lock.deploy.config.threatbus.version}
