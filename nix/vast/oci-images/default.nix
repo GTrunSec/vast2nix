30c012d101f160488ffaef38e1bbba7ede1aef97
GTrunSec <gtrunsec@hardenedlinux.org>
2022-11-03 16:38:59 -0700

    DONE OCI-image with docker/podman

--- nix/vast/oci-images/default.nix
+++ nix/vast/oci-images/default.nix
@@ -7,27 +7,36 @@
 
   l = inputs.nixpkgs.lib // builtins;
 in {
+  /*
+  docker run --user $(id -u):$(id -g) -it --rm -p 42000:42000 -v $(pwd)/test \
+  docker.io/library/vast-release
+
+  # https://www.redhat.com/sysadmin/debug-rootless-podman-mounted-volumes
+
+  podman run -it --rm --userns=keep-id --group-add keep-groups --user vast \
+  -p 42000:42000 -v $(pwd)/test:/var/lib/vast docker.io/library/vast-release
+  */
   prod = mkStandardOCI rec {
     name = "vast-release";
     operable = cell.operables.prod;
     options = {
-      config.Volumes = {
-        "/var/lib/vast" = {};
-      };
+      # config.Volumes = {
+      #   "vast:/var/lib/vast" = {};
+      # };
       config.WorkingDir = "/var/lib/vast";
       config.ExposedPorts = {
         "42000/tcp" = {};
       };
     };
     setup = [
-      nixpkgs.coreutils
+      # nixpkgs.coreutils
       nixpkgs.bash
       (mkUser {
         user = "vast";
         group = "vast";
-        uid = "1001";
-        gid = "1001";
-        withHome = true;
+        uid = "1000";
+        gid = "1000";
+        # withHome = true;
         # withRoot = true;
       })
       (mkSetup "logs" [
@@ -36,12 +45,12 @@ in {
             regex = "server.log";
             mode = "0777";
           }
-          {
-            regex = "/var/lib/vast";
-            mode = "0744";
-            user = "vast";
-            group = "vast";
-          }
+          # {
+          #   regex = "/var/lib/vast";
+          #   mode = "0744";
+          #   user = "vast";
+          #   group = "vast";
+          # }
         ] ''
           mkdir -p $out/var/log/vast
           touch $out/var/log/vast/server.log
