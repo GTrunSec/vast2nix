c1050c93b02760c020e3167c8dec0a28a4a0fb2b
GTrunSec <gtrunsec@hardenedlinux.org>
2022-12-30 22:24:05 -0800

    Update

--- nix/common/lib.nix
+++ nix/common/lib.nix
@@ -5,7 +5,7 @@
   inherit (inputs.cells-lab.writers.lib) writeConfiguration;
   inherit (inputs) std self cells-lab;
 
-  __inputs__ = cells-lab.common.lib.callFlake ./lock {
+  __inputs__ = cells-lab.common.lib.callFlake ./lib/lock {
     nixpkgs.locked = inputs.nixpkgs.sourceInfo;
     nixpkgs-hardenedlinux.inputs.nixpkgs = "nixpkgs";
   };
